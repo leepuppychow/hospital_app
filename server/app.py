daaca9867184f98857eba123504ac13d4dd87944
leepuppychow <leepuppychow@gmail.com>
2021-06-14 00:44:45 -0600

    Adds endpoints to create,  update, and delete hospitals

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -22,15 +22,52 @@ def ping():
 @app.route('/api/v1/hospitals', methods=['GET'])
 def get_all_hospitals():
     try:
-        hospitals = [hospital.to_json for hospital in Hospital.all()]
-        return jsonify(hospitals), 200
+        hospitals = Hospital.all()
+        return jsonify([h.to_json for h in hospitals]), 200
     except Exception as e:
         return jsonify({"message": f"Error getting hospitals data: {str(e)}"}), 400
 
 @app.route('/api/v1/hospitals/<id_hospital>', methods=['GET'])
 def get_one_hospital(id_hospital):
     try:
-        hospital = Hospital.find(id_hospital).to_json
-        return jsonify(hospital), 200
+        hospital = Hospital.find(id_hospital)
+        return jsonify(hospital.to_json), 200
     except Exception as e:
         return jsonify({"message": f"Error getting hospital data: {str(e)}"}), 400
+
+@app.route('/api/v1/hospitals', methods=['POST'])
+def create_hospital():
+    try:
+        hospital = Hospital.create(request.json)
+        db.session.add(hospital)
+        db.session.commit()
+        return jsonify({
+            "message": f"Successfully created hospital: {hospital.id}",
+            "hospital": hospital.to_json,
+        }), 201
+    except Exception as e:
+        return jsonify({"message": f"Error creating hospital: {str(e)}"}), 400
+
+@app.route('/api/v1/hospitals/<id_hospital>', methods=['PUT'])
+def update_hospital(id_hospital):
+    try:
+        hospital = Hospital.update(id_hospital, request.json)
+        db.session.commit()
+        return jsonify({
+            "message": f"Successfully updated hospital: {hospital.id}",
+            "hospital": hospital.to_json,
+        }), 200
+    except Exception as e:
+        return jsonify({"message": f"Error updating hospital data: {str(e)}"}), 400
+
+@app.route('/api/v1/hospitals/<id_hospital>', methods=['DELETE'])
+def delete_hospital(id_hospital):
+    try:
+        hospital = Hospital.find(id_hospital)
+        db.session.delete(hospital)
+        db.session.commit()
+        return jsonify({
+            "message": f"Successfully deleted hospital: {id_hospital}",
+        }), 200
+    except Exception as e:
+        return jsonify({"message": f"Error deleting hospital: {str(e)}"}), 400
