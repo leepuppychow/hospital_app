99f84976aed633f8d0170aec6f0dc412c52d39fd
leepuppychow <leepuppychow@gmail.com>
2021-06-14 10:11:39 -0600

    Adds another hospital in seed script

--- server/manage.py
+++ server/manage.py
@@ -9,13 +9,14 @@ def seed():
   print("~~~ Begin seeding data ~~~")
   hospital_1 = Hospital("Hospital 1", "123 test st denver, CO 80000")
   hospital_2 = Hospital("Hospital 2")
+  hospital_3 = Hospital("Hospital Three")
   patient_1 = Patient('patient', 'one')
   patient_2 = Patient('patient', 'two')
   patient_3 = Patient('patient', 'three')
   hospital_1.patients = [patient_1, patient_2]
   hospital_2.patients = [patient_3]
 
-  db.session.add_all([hospital_1, hospital_2])
+  db.session.add_all([hospital_1, hospital_2, hospital_3])
   db.session.commit()
   
   print("~~~ Finished seeding data ~~~")
