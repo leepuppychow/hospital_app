2544fe83d67001ebd95e2d7eb595c0e1f6d58d69
leepuppychow <leepuppychow@gmail.com>
2021-06-14 00:21:58 -0600

    Adds to_json property on Patient

--- server/models/patient.py
+++ server/models/patient.py
@@ -11,3 +11,11 @@ class Patient(db.Model):
   def __init__(self, first_name, last_name):
     self.first_name = first_name
     self.last_name = last_name
+
+  @property
+  def to_json(self):
+    return {
+        "id": self.id,
+        "first_name": self.first_name,
+        "last_name": self.last_name,
+    }
