79b9e4e3fec01b805aaaa6ab39dea49fa20275b7
leepuppychow <leepuppychow@gmail.com>
2021-06-14 10:08:15 -0600

    Sets up basic react router for Patients and Hospitals index

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import HospitalIndex from "./components/hospital/HospitalIndex";
+import PatientIndex from "./components/patient/PatientIndex";
+import { getHospitals } from './services/requestService'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hospitals: [],
+    }
+    this.reloadHospitalData = this.reloadHospitalData.bind(this);
+  }
+
+  async componentDidMount() {
+    this.reloadHospitalData();
+  }
+
+  async reloadHospitalData() {
+    try {
+      const res = await getHospitals();
+      if (res.data) this.setState({hospitals: res.data});
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/hospitals">Hospitals</Link>
+              </li>
+              <li>
+                <Link to="/patients">Patients</Link>
+              </li>
+            </ul>
+          </nav>
+  
+          <Switch>
+            <Route
+              path="/hospitals"
+              render={() => (
+                <HospitalIndex
+                  hospitals={this.state.hospitals}
+                  reload={this.reloadHospitalData}
+                />
+              )}
+            />
+            <Route
+              path="/patients"
+              render={() => (
+                <PatientIndex 
+                  hospitals={this.state.hospitals}
+                  reload={this.reloadHospitalData}
+                />
+              )}
+            />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
-
-export default App;
